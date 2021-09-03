6047c31803e63e591ec61d60b8f620d7379d0e9c
Sylwester <zieemer@gmail.com>
2021-09-03 09:56:40 +0100

    add db models

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
@@ -6,6 +7,7 @@ namespace StarChart.Data
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-        }
+            public DbSet<CelestialObject> CelestialObjects { get; set; }
+    
     }
 }
