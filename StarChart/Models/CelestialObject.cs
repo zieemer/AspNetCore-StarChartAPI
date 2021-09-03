b8151c06f158b0ca351f51ac4b59db267805f8d0
Sylwester <zieemer@gmail.com>
2021-09-03 10:10:58 +0100

    fix for Satellites

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -15,7 +15,7 @@ namespace StarChart.Models
 
         public int? OrbitedObjectId { get; set; }
         [NotMapped]
-        public List<CelestialObject> Satelities { get; set; }
+        public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
     }
 }
