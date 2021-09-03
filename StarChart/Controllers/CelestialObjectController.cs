751a00839436931fb39708b4bd3f8fd91c5535be
Sylwester <zieemer@gmail.com>
2021-09-03 14:02:23 +0100

    test fix

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,9 +17,6 @@ namespace StarChart.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+       
     }
 }
